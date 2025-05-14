8c6461ecfa5bb7814dfa11350d051e8b96aebb15
Cutieoff <162575012+Cutieoff@users.noreply.github.com>
2025-05-14 22:46:24 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -325,7 +325,7 @@ Aᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs:
  </b>""" 
 
     PROGRESS_BAR = """\n
-╭━━━━❰ Tech VJ Renaming... ❱━➣
+╭━━━━❰  MT Renaming... ❱━➣
 ┣⪼ 🗂️ : {1} | {2}
 ┣⪼ ⏳️ : {0}%
 ┣⪼ 🚀 : {3}/s
@@ -703,9 +703,9 @@ Also your tutorial will be Added Your Group..."""
     DEVELOPER_TXT = """
 special Thanks To ❤️ Developers -
 
--Dev 1 [Owner of this bot ]<a href='https://t.me/@KANNADA_KID'>KK</a>
+-Dev 1 [Owner of this bot ]<a href='https://t.me/@Mall00003'>KK</a>
 
--Dev 2 <a href='https://t.me/@KANNADA_KID'>KK</a>
+-Dev 2 <a href='https://t.me/@Mall00003'>KK</a>
 
 -Dev 3 <a href='https://t.me/vj_botz>VJ Botz</a>
 
